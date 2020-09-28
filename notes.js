cfefecadbb3e127b23e7dbfa29b308e7735a2d87
Matthew Denune <mattdenune@gmail.com>
2020-09-28 19:23:04 -0400

    added ability to store note

--- notes.js
+++ notes.js
@@ -1,7 +1,45 @@
-
+const fs = require('fs');
+const chalk = require('chalk');
 
 const getNotes = function(str) {
     return str
 }
 
-module.exports = getNotes;
+const addNote = function(title, body) {
+    const notes = loadNotes()
+    const duplicateNotes = notes.filter(function(note) {
+        return note.title === title
+    })
+
+    if (duplicateNotes.length === 0) {
+        notes.push({
+            title: title,
+            body: body
+        })
+        saveNotes(notes)
+        console.log(chalk.green('New note added!'))
+    } else {
+        console.log(chalk.red('Note title taken'))
+    }
+}
+
+const saveNotes = function(notes){
+    const dataJSON = JSON.stringify(notes)
+    fs.writeFileSync('notes.json', dataJSON)
+}
+
+const loadNotes = function() {
+    try{
+        const dataBuffer = fs.readFileSync('notes.json')
+        const dataJSON = dataBuffer.toString()
+        return JSON.parse(dataJSON)
+    } catch(e) {
+        return []
+    }
+}
+
+
+module.exports = {
+    getNotes: getNotes,
+    addNote: addNote
+}
